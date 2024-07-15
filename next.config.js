a84f183a677a0771d14208980c027c549ca775cc
kamakshi <kamakshit2302@gmail.com>
2024-07-15 19:25:34 -0400

    finish

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    future: {
-      webpack5: true, // If using Next.js 11 or above
-    },
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback.fs = false; // Required to avoid a webpack5 issue
-      }
-      return config;
-    },
-  };
-  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false, // Avoid a webpack 5 issue
+      };
+    }
+    return config;
+  },
+};
